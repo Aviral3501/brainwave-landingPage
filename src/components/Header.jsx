8ca29afb80c62f3e38c404ea0ea81ab47ad0e167
Aviral Singh <aviralsingh3501@gmail.com>
2024-03-09 07:24:52 +0530

    Change the background color of the header when the navigation menu is open.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
     }
    
     return (
-        <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
+        <div className={`fixed top-0 left-0 w-full z-50 border-b 
          border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8":"bg-n-8/90 backdrop-blur-sm"} `}>
             <div className="flex items-center  px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
                 <a className="block w-[12rem] xl:mr-8" href="#hero">
@@ -36,7 +36,7 @@ const Header = () => {
                     <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                         {/* bringin the nav elements from  constants file */}
                         {navigation.map((item) => (
-                            <a key={item.id} href={item.url}
+                            <a key={item.id} href={item.url}  onClick={handleClick}
                                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""}
                           px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === pathName.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/35"}
                            lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
