57b249a1c293e0d96e125badff22b5787d32d1d6
Aviral Singh <aviralsingh3501@gmail.com>
2024-03-17 07:35:24 +0530

    checkpoint

---
+++ src/components/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Section from './Section'
+
+const Services = () => {
+  return (
+    <Section crosses>
+        <div className='container'>
+        Services section
+        </div>
+    </Section>
+   
+  )
+}
+
+export default Services
